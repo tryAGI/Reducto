6104c46944e7aa61cf46c7939fdc1ee5d1a73797
Konstantin S. <havendv@gmail.com>
2026-04-03 09:07:06 +0400

    feat: Updated OpenAPI spec (#6)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Reducto/Generated/Reducto.ReductoClient.WebhookPortalConfigureWebhookPost.g.cs
+++ src/libs/Reducto/Generated/Reducto.ReductoClient.WebhookPortalConfigureWebhookPost.g.cs
@@ -160,7 +160,6 @@ namespace Reducto
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
